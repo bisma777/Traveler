3c13bcbd5c524dcb9e9ec4186f31fecc4f926c6a
bisma777 <nazirbisma7@gmail.com>
2022-01-22 23:41:06 +0530

    change made 2.0

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import {Paper,Typography,useMediaQuery} from '@material-ui/core';
+import {Paper,Typography} from '@material-ui/core';
+
 
 import Rating from '@material-ui/lab/Rating';
 
@@ -9,6 +10,7 @@ import useStyles from './styles';
 
 function Map({setCoordinates,setBounds,coordinates,places}) {
     const classes = useStyles();
+  
    
   return (
       <div className={classes.mapContainer}>
@@ -22,15 +24,17 @@ function Map({setCoordinates,setBounds,coordinates,places}) {
         setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw})
       }}
       >
-          {places?.map((place,i)=>(
-            <div
-                className={classes.marketContainer}
-                lat={Number(place.latitude)}
-                lng={Number(place.longitude)}
-                key={i}
+      {places?.map((place,i)=>(
+        <div
+            className={classes.marketContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
 
-            >
-                  <Paper elevation={3} className={classes.paper}>
+        >
+          
+           
+              <Paper elevation={3} className={classes.paper}>
               <Typography className={classes.typography} variant='subtitle2' gutterBottom>{place.name}</Typography>
                 <img 
                   className={classes.pointer}
